9b9d110c27b312d76e49e06c97d1ec02ea7edb8a
Juan David Sáenz <75400465+judas1221@users.noreply.github.com>
2024-07-29 20:46:42 -0500

    conecction with database

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 import express from 'express';
 const app = express();
+import mongoose  from 'mongoose';
 
 app.get('/', (req, res)=>{
     res.send('prueba 1 respuesta del servidor');
 });
+
+const password= 'E34ukEI0PWfEGKqo';
+const url= "mongodb+srv://judasaor:E34ukEI0PWfEGKqo@cluster0.4mp7l7w.mongodb.net/?retryWrites=true&w=majority&appName=cluster0";
+
+main().catch(err => console.log(err));
+async function main() {
+    await mongoose.connect(url)
+    console.log('Se ha conectado con la base de datos');
+
+}
 
 app.listen(10000);
